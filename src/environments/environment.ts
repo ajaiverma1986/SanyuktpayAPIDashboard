ba4cc0d20e081a0d03abffbddb847bd5d9cffb10
ajaiverma1986 <45514244+ajaiverma1986@users.noreply.github.com>
2024-09-04 23:01:42 +0530

    check in 20240904 night

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     Production:true,
-     baseurl:"https://uatapi.sanyuktpay.in",
-   // baseurl:"http://localhost:31130",
+    // baseurl:" https://api.sanyuktpay.in",
+    // baseurl:"https://uatapi.sanyuktpay.in",
+    baseurl:"http://localhost:31130",
     APIToken:"48550088-F090-495D-9F28-597E5FE22D6A"
 };
