ccd06c9a2806cba2e352252d478e91422ab42ccc
ajaiverma1986 <45514244+ajaiverma1986@users.noreply.github.com>
2024-09-23 22:41:09 +0530

    check in 20240923 night

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './component/common/login/login.component';
 
@@ -9,6 +9,25 @@ import { LoginComponent } from './component/common/login/login.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'SanyuktDashboard';
+  lng!:any;
+  lat!:any;
+
+  ngOnInit(): void {
+    //this.getUserLocation();
+  }
+
+  getUserLocation() {
+    if (navigator.geolocation) {
+     navigator.geolocation.getCurrentPosition(position => {
+         this.lat = position.coords.latitude;
+         this.lng = position.coords.longitude;
+       });
+ }else {
+    console.log("User not allow")
+
+ }
+}
 }
