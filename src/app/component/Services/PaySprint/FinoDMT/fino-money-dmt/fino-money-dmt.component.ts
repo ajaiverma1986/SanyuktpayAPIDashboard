35a63eaa9a7450b4c5cdba783a7c91e3fc5c2166
Ajai Verma <ajai.verma@fiaglobal.com>
2025-03-20 22:06:32 +0530

    check in 20250320

--- src/app/component/Services/PaySprint/FinoDMT/fino-money-dmt/fino-money-dmt.component.ts
+++ src/app/component/Services/PaySprint/FinoDMT/fino-money-dmt/fino-money-dmt.component.ts
@@ -4,27 +4,35 @@ import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { FinoDMTService } from '../../../../../services/PaysprintServcies/fino-dmt.service';
 import { GetCustomerRequestView } from '../../../../../RequestModel/SpayModel/FinoDMTRequest';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-fino-money-dmt',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './fino-money-dmt.component.html',
   styleUrl: './fino-money-dmt.component.scss'
 })
 export class FinoMoneyDMTComponent extends BasecomponentComponent implements OnInit {
   Usertoken!: string;
   CustModel: GetCustomerRequestView = new GetCustomerRequestView();
+  frmVerifyCust!:FormGroup;
+  strmsg!:string;
 
-  constructor(private routs: Router, private dmt: FinoDMTService, toster: ToastrService) {
+  constructor(private routs: Router, private dmt: FinoDMTService,private fb:FormBuilder, toster: ToastrService) {
     super(toster)
   }
   ngOnInit(): void {
 
+    this.createForm();
     this.GenerateToken();
-   
-    this.GetCustomerDetail();
+  
   }
+   createForm() {
+      this.frmVerifyCust = this.fb.group({
+        MobileNo: ['', [Validators.required]],
+      });
+    }
   GenerateToken() {
     this.dmt.GenerateSPAYToken().subscribe({
       next: (data) => {
@@ -41,13 +49,35 @@ export class FinoMoneyDMTComponent extends BasecomponentComponent implements OnI
     });
   }
   GetCustomerDetail() {
-    this.CustModel.Mobile = "9716232890";
+    this.CustModel.Mobile = this.frmVerifyCust.get("MobileNo")?.value;
     this.CustModel.TokenData = sessionStorage.getItem("PaySPTOKEN") || '';
     
     this.dmt.GetCustomerDetail(this.CustModel).subscribe({
       next: (result) => {
-        console.log(result);
+
+        this.strmsg=result.message || '';
+        if(result.response_code=="1")
+        {
+this.showToaster(1,this.strmsg,"DMT")
+        }
+        else
+        {
+          this.showToaster(3,this.strmsg,"DMT")
+        }
       }
     });
+  }
+  getInvalidControls() {
+    const invalidControls = [];
+    const controls = this.frmVerifyCust.controls;
+    for (const name in controls) {
+      if (controls[name].invalid && controls[name].touched) {
+        invalidControls.push(name);
+      }
+    }
+    return invalidControls;
+  }
+  onSubmit(){
+    this.GetCustomerDetail();
   }
 }
